02357268d59212f1bd846268c21160281deef6d1
Aayush Mishra <manofganges@gmail.com>
2025-06-13 18:55:51 +0530

    Update Post.vue

--- components/blog/Post.vue
+++ components/blog/Post.vue
@@ -46,7 +46,7 @@ const props = defineProps<{
             alt=""
           />
           <div>
-            <a href="#">@AayushMishra</a>
+            <a href="#">@aayushmishra</a>
           </div>
         </div>
         <div class="flex">
