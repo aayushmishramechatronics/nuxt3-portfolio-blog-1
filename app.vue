f3db175418f9977d0742a285710e58691a8418cc
Aayush Mishra. <manofganges@gmail.com>
2025-08-13 01:59:04 +0530

    Update app.vue

--- app.vue
+++ app.vue
@@ -5,14 +5,6 @@
   </NuxtLayout>
 </template>
 
-<script setup>
-// Initialize the blog stats store on app mount
-const blogStatsStore = useBlogStatsStore()
-
-onMounted(() => {
-  blogStatsStore.loadStats()
-})
-</script>
 
 
 
